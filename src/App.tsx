22fc7e73deb5db6508be7dc512c2a6a13982a9e9
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-02-23 20:25:51 +0700

    useState, useEffect, children props use ts-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 
 const Heading = ({ title }: { title: string }) => {
   return <h2 className="font-primary font-bold text-3xl mb-5">{title}</h2>;
@@ -8,6 +8,10 @@ type ActionType =
   | { type: "ADD"; text: string }
   | { type: "REMOVE"; id: number };
 interface Todo {
+  id: number;
+  text: string;
+}
+interface Data {
   id: number;
   text: string;
 }
@@ -28,7 +32,6 @@ const todoReducer = (state: Todo[], action: ActionType) => {
       throw new Error("");
   }
 };
-
 const initialState: Todo[] = [];
 const App = () => {
   const [toDos, dispatch] = useReducer(todoReducer, initialState);
@@ -49,9 +52,33 @@ const App = () => {
       inputRef.current.focus();
     }
   };
+  const [data, setData] = useState<Data | null>(null);
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+      });
+  }, []);
+  const onClickItem = (item: string) => {
+    alert(item);
+  };
   return (
     <div>
       <Heading title="Todo App"></Heading>
+      {JSON.stringify(data)}
+      <List
+        items={["javascript", "nodejs", "react"]}
+        onClickItem={(item: string) => onClickItem(item)}
+      ></List>
+      <Boxed>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolorum
+          quibusdam amet minus consectetur qui, consequuntur magni! Corporis,
+          cupiditate natus? Repellendus deserunt porro ducimus ipsum ab corporis
+          doloremque laboriosam dolorum.
+        </div>
+      </Boxed>
       <div className="max-w-sm">
         <div className="flex items-center gap-x-5">
           <input
@@ -82,6 +109,32 @@ const App = () => {
       </div>
     </div>
   );
+};
+
+const List = ({
+  items,
+  onClickItem,
+}: {
+  items: string[];
+  onClickItem?: (item: string) => void;
+}) => {
+  return (
+    <div>
+      {items.map((item) => (
+        <div
+          key={item}
+          onClick={() => onClickItem?.(item)}
+          className="cursor-pointer"
+        >
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Boxed = ({ children }: { children?: React.ReactNode }) => {
+  return <div>{children}</div>;
 };
 
 export default App;
